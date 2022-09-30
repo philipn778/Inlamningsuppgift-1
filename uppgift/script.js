36a049dfbca86af2a96c9d91944b029df7aeaf9b
Philip <philipncontact@gmail.com>
2022-09-30 16:07:47 +0200

    script.js

--- uppgift/script.js
+++ uppgift/script.js
@@ -1,16 +1,20 @@
 let btn = document.getElementById('btn');
 let output = document.getElementById('outputtext');
 
-let number = [Math.floor(Math.random() * 100)] //generar ett nummer mellan 0 - 100
+let number = [Math.floor(Math.random() * 100)] //generar ett nummer mellan 1 - 100
 
-btn.addEventListener('click', function() {
+btn.addEventListener('click', function(){
     let input = document.getElementById('userInput').value;
     if (input==number){
         output.innerHTML = `Du gissade rätt, din gissning var ${number}`
+        console.log('Du gissade rätt!');
     } else if (input < number){
         output.innerHTML = 'Du gissade för lågt!'
+        console.log('Du gissade för lågt!');
     };
     if (input > number){
         output.innerHTML = 'Du gissade för högt!'
+        console.log('Du gissade för högt!');
     }
 });
+
